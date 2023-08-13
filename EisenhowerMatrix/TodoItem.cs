5da3161355a65f321fec2fd8cbdda2f34d11e248
Kaszydo <kaszydo@gmail.com>
2023-08-13 12:05:41 +0200

    [Feature] ToDoItem Class. ToDoItem has title, deadline, iddone. Methods to mark unmark task as done. Tostring() method.

--- EisenhowerMatrix/TodoItem.cs
+++ EisenhowerMatrix/TodoItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,36 @@ namespace EisenhowerMain
 {
     internal class TodoItem
     {
+        private readonly string Title;
+
+        private readonly DateTime Deadline;
+
+        public bool IsDone;
+
+        public TodoItem(string title, DateTime deadline)
+        {
+            Title = title;
+            Deadline = deadline;
+            IsDone = false;
+        }
+
+        private string GetTitle() { return Title; }
+        private DateTime GetDeadLine() { return Deadline; }
+        public void Mark() { IsDone = true; }
+        public void Unmark() { IsDone = false; }
+        public override string ToString()
+        {
+            string title = GetTitle();
+            string deadline = GetDeadLine().ToString("dd-MM");
+            string mark = "[ ]";
+            if (IsDone)
+            {
+                mark = "[X]";
+            }
+            string task = string.Concat(mark, " ", deadline, " ", title);
+            return task;
+        }
+
+
     }
 }

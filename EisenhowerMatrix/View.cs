11628e7b0a11d25c8992ac34670a79e45c98d940
Kaszydo <kaszydo@gmail.com>
2023-08-20 11:13:33 +0200

    [Feature] Input and View classes MainScrean Shows matrix and available menu options

---
+++ EisenhowerMatrix/View.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EisenhowerMain
+{
+    internal class View
+    {
+        public View() { }
+        public void MainScrean(TodoMatrix matrix)
+        {
+            Console.WriteLine(matrix.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine("1. Add New Item");
+            Console.WriteLine("2. Mark/Unmark Task");
+            Console.WriteLine("3. Archive");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine();
+            Console.Write("Chose option: ");
+        }
+    }
+}

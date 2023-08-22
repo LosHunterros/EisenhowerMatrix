15b1ca1cb348261cdd6f9e84a5d8b3bd379f2cb5
Kaszydo <kaszydo@gmail.com>
2023-08-22 17:45:40 +0200

    {Feature] Menu options enum

---
+++ EisenhowerMatrix/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EisenhowerMain
+{
+    public enum Menu
+    {
+        Add,
+        Edit,
+        Delete,
+        Mark,   
+        Archive,
+        Exit
+    }
+}
